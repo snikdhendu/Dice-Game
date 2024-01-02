ae4072ca8788dc35dfea770abedfa4f9226ba8d1
Snikdhendu Pramanik <136674597+snikdhendu@users.noreply.github.com>
2024-01-02 20:47:09 +0530

    Update GameComponent.jsx

--- src/Components/GameComponent.jsx
+++ src/Components/GameComponent.jsx
@@ -9,7 +9,7 @@ const GameComponent = () => {
   };
 
   return (
-    <div className='relative bottom-72 left-40 z-10 border-3 w-20'>
+    <div className='relative bottom-72 md:left-40 left-10 z-10 border-3 w-20'>
       <div className="flex items-center justify-center">
         <button type="button" onClick={toggleInstructions} class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
           
