dd5297de64d7c7e76fd4f688305f73db60f95f2b
Snikdhendu Pramanik <136674597+snikdhendu@users.noreply.github.com>
2024-01-05 10:06:17 +0530

    Update Rolldice.jsx

--- src/Components/Rolldice.jsx
+++ src/Components/Rolldice.jsx
@@ -28,7 +28,7 @@ const Rolldice = ({ currentDice, currentDice2, roleDice, isRolling }) => {
 
 
       </div>
-      <div><button class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-32" onClick={roleDice} >click</button></div>
+      <div><button class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-32" onClick={roleDice} >click to Roll</button></div>
     </div>
   );
 };
