0bb4e8de3a5a6518480c565222e74a51485a1250
Snikdhendu Pramanik <136674597+snikdhendu@users.noreply.github.com>
2024-01-05 09:44:41 +0530

    Update Topsection.jsx

--- src/Components/Topsection.jsx
+++ src/Components/Topsection.jsx
@@ -5,16 +5,16 @@ import { useState } from 'react';
 
 const Topsection = ({selectedNumber,setSelectedNumber,score,error}) => {
 
-    const arrNumber = [1, 2, 3, 4, 5, 6];
+    const arrNumber = [2,3,4,5,6,7,8,9,10,11,12];
    
     console.log(selectedNumber)
     return (
-        <div className='w-full h-52  p-14 flex md:flex-row flex-col items-center justify-between'>
+        <div className='w-full md:h-52 sm:h-52 h-64  p-14 flex md:flex-row flex-col items-center justify-between'>
             <div className='md:text-4xl text-3xl text-slate-50'>Total Score : { score}</div>
-            <div className='w-96  h-auto flex justify-between items-center flex-col gap-4'>
-                <h1 className=' text-xl text-red-600'>{error}</h1>
-                <h1 className='text-3xl text-slate-50'>Choose Any Number : </h1>
-                <div className='md:w-96 w-80 h-auto flex justify-between items-center button-con'>
+            <div className='w-full md:w-1/2  h-auto flex justify-between items-center flex-col gap-4'>
+                <h1 className=' md:text-xl sm:text-xl text-base mt-4 md:mt-0 sm:mt-0 text-red-600'>{error}</h1>
+                <h1 className='md:text-3xl sm:text-3xl text-2xl  text-slate-50'>Choose Any Number : </h1>
+                <div className='md:w-full w-80 flex-wrap h-auto flex justify-normal items-center button-con gap-2'>
                     {arrNumber.map((value, i) => (
                         <button
                             key={i}
